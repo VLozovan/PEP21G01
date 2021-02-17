622431a4d1c637fa0f3c2be6b5ca9383b949adbf
Vas <vasile.lozovan@gmail.com>
2021-02-17 21:35:40 +0200

    test 1

---
+++ modul1/modul1_1.py
@@ -0,0 +1,23 @@
+print('print\u200b("")')
+# print​("")
+print(print)
+print(3)
+print('my name is:','vasile')
+print('My name is:','vasile','1',sep='_',end='*')
+print('My name is:','vasile','2',sep='_',end='*')
+print('My name is:','vasile','3',sep='_',end='*')
+print()
+
+print_text1 = 'Hello World 1'
+print_text2 = 'Hello World 2'
+
+print(print_text1, print_text2)
+
+print_text1 = 123
+print_text2 = 456
+
+print(print_text1, print_text2)
+
+print('*   *** *  ')
+print('*   * * *  ')
+print('*** *** ***')
