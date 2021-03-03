7c14519a0f6888fbc122648d11c3c4ecab005566
Vas <vasile.lozovan@gmail.com>
2021-03-03 23:58:21 +0200

    test 1

---
+++ modul3/modul3_1.py
@@ -0,0 +1,38 @@
+# n = int(input('un numar:'))
+# if n < 10:
+#     print('prea mic')
+# elif n > 10:
+#     print('prea mare')
+# else:
+#     print('just right')
+
+# type
+
+my_string = 'text'
+print(type(my_string))
+
+my_type = type(my_string)
+print(type(my_type))
+
+print(type(my_string) == str)
+
+my_string = 6
+print(type(my_string))
+
+my_type = type(my_string)
+print(type(my_type))
+
+print(type(my_string) == str)
+
+# n = input('Input:')
+# if 47 < ord(n[0]):
+#     print(type(n))
+#     if ord(n[0])<58:
+#         result = int(n)
+# print(result)
+
+# n = input('input:')
+# if n:
+#      print("Succes")
+
+# for loops
